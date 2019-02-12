1e9beb2c26a1c457cdbc1e3982866724fa1de467
noahrudin <noahrudin@gmail.com>
2019-02-12 10:33:57 -0700

    added account screen

---
+++ src/app/account/account.page.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'account.page.html',
+  styleUrls: ['account.page.scss'],
+})
+export class AccountPage {
+
+}
