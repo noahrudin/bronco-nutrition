ea44bd20f0d6bb536f9f831c54faf6ba26440638
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2019-02-12 20:37:37 -0700

    push to master

--- pushScript.sh
+++ pushScript.sh
@@ -1,4 +1,8 @@
-		git add .
-		git commit -m "Push script"
-		git pull --rebase
-		git push origin master
+
+	rm -rf node_modules
+	git add .
+	git commit -m "push to master"
+	git pull --rebase
+	git push origin master
+	npm install
+
