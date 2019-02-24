04f8d0d6882f94182d18fe3210e809408fbe14d9
AFoy95 <38448102+AFoy95@users.noreply.github.com>
2019-02-24 00:01:49 -0700

    updating database/firebase

--- src/app/account/account.page.ts
+++ src/app/account/account.page.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-
+import { FirebaseService } from '../database/firebase.service';
 @Component({
   selector: 'app-home',
   templateUrl: 'account.page.html',
   styleUrls: ['account.page.scss'],
 })
 export class AccountPage {
-    
-    constructor() {
+
+    constructor(private fireService: FirebaseService) {
 
     }
 }
--- src/app/database/firebase.service.ts
+++ src/app/database/firebase.service.ts
@@ -1,15 +1,18 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireList, snapshotChanges } from '@angular/fire/database';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class FirebaseService {
-    lists: AngularFireList<any>;
+    users: AngularFireList<any>;
+    private email;
+    private firstname;
+    private lastname;
     constructor(private afData: AngularFireDatabase) {
         afData.database.ref('/Users');
-        this.lists = afData.list('Users/');
+        this.users = afData.list('Users/');
     }
 
     createUser(email: string, firstname: string, lastname: string) {
@@ -20,13 +23,16 @@ export class FirebaseService {
         });
     }
 
-    getUser() {
-        this.afData.database.ref("Users/").on('value', function (snapshot) {
-            var User = snapshot.val();
-        });
+   get getUser() {
+       return this.afData.database.ref("Users/").on('value', () => {
+           this.email = this.afData.object('Users/email');
+           this.firstname = this.afData.object('Users/firstname');
+           this.lastname = this.afData.object('Users/lastname');
+       });
 
     }
     delete() {
 
     }
 }
+
