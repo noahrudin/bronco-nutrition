a8fb074d1f9cb706473be72782d669933555e91f
noahrudin <noahrudin@gmail.com>
2019-03-05 18:42:42 -0700

    browse page overhaul

--- src/app/browse/browse.page.ts
+++ src/app/browse/browse.page.ts
@@ -2,6 +2,7 @@ import { Component, Input } from '@angular/core';
 import { NavController } from '@ionic/angular';
 import * as firebase from 'firebase';
 import { InputChangeEventDetail } from '@ionic/core';
+
 @Component({
   selector: 'app-home',
   templateUrl: 'browse.page.html',
@@ -11,35 +12,34 @@ export class BrowsePage{
     private recipedb: firebase.database.Reference = firebase.database().ref('masterSheet/');
     private fooddb: firebase.database.Reference = firebase.database().ref('foodSheet/');
     public loadedRecipeList: Array<any>;
+    public componentRecipe: any;
     public recipeList: Array<any>;
   constructor( 
       private navCtrl: NavController) {
-      
+
+      this.recipedb.on('value', recipeList => {
+          let recipes = [];
+              recipeList.forEach(recipe => {
+                  recipes.push(recipe.val());
+                  return false;
+              });
+          this.recipeList = recipes;
+          this.loadedRecipeList = recipes;
+      });
 
   }
 
-    recipeButtonClick() {
-      this.navCtrl.navigateForward('recipelist');
-    }
+  //Austin, can you have this act like the selecting 
+  //list component in recipelist.page.ts?
+  listItemClick(index: number) {
+    //RecipeListPage.selectedRecipe = this.recipes[index + 1];
+    this.navCtrl.navigateForward('recipedetails');
+  }
 
-    foodButtonClick() {
+    foodListClick() {
       this.navCtrl.navigateForward('foodlist');
     }
-    drawRecipes(search){
-        console.log(this.recipedb.startAt(0));
-        this.recipedb.on('value', recipeList => {
-            let recipes = [];
-            if(search != "empty"){
-            recipeList.forEach(recipe => {
-                recipes.push(recipe.val());
-                return false;
-            });
-        }
-            this.recipeList = recipes;
-            this.loadedRecipeList = recipes;
-        });
-    
-    }
+   
     initializeRecipes() {
             this.recipeList = this.loadedRecipeList;
     }
@@ -48,16 +48,11 @@ export class BrowsePage{
         
         var search = searchbar.srcElement.value;
         this.initializeRecipes();
-        if (!search) {
-        this.drawRecipes("empty");
-            return ;
-        }
-        this.drawRecipes(search);
         
         this.recipeList = this.recipeList.filter((val) => {
             if (val && search) {
-                if ((val[0].indexOf(search)>-1)||(val[0].toLowerCase().indexOf(search)>-1)) {
-                  
+                if ((val[0].indexOf(search) > -1) || (val[0].toLowerCase().indexOf(search) > -1)) {
+                    this.componentRecipe = search;
                     return true;
                 }
                 return false;
@@ -67,4 +62,8 @@ export class BrowsePage{
         
        // console.log(search, this.recipeList.length);
     }
+
+    
+        
+    
 }
