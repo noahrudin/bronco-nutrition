be568532e824574226b848bff146d57792f44f37
twolfe21 <vonwolfee@gmail.com>
2019-03-26 09:22:50 -0600

    minor lint fixes in auth module

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -19,22 +19,28 @@ const STEPS_INDEX = 5;
 
 export const snapshotToRecipeArray = snapshot => {
   const returnArr = [];
-  let idx: number = 0;
+  let idx = 0;
   snapshot.forEach(childSnapshot => {
-      const item = childSnapshot.val();
-      // package everything up into an easily usable Recipe object.
-      const title = item[RECIPE_NAME_INDEX];
-      const numServings = Recipe.parseNumServings(item[NUM_SERVINGS_INDEX]);
-      const macros = Recipe.parseMacros(item[MACROS_INDEX]);
-      const prepTime = item[PREP_TIME_INDEX];
-      const ingredients = Recipe.parseIngredients(item[INGREDIENTS_INDEX]);
-      const steps = Recipe.parseSteps(item[STEPS_INDEX]);
+    const item = childSnapshot.val();
+    // package everything up into an easily usable Recipe object.
+    const title = item[RECIPE_NAME_INDEX];
+    const numServings = Recipe.parseNumServings(item[NUM_SERVINGS_INDEX]);
+    const macros = Recipe.parseMacros(item[MACROS_INDEX]);
+    const prepTime = item[PREP_TIME_INDEX];
+    const ingredients = Recipe.parseIngredients(item[INGREDIENTS_INDEX]);
+    const steps = Recipe.parseSteps(item[STEPS_INDEX]);
 
-      const newRecipe = new Recipe(title, numServings, macros,
-                                  prepTime, ingredients,
-                                  steps, idx);
-      returnArr.push(newRecipe);
-      idx++;
+    const newRecipe = new Recipe(
+      title,
+      numServings,
+      macros,
+      prepTime,
+      ingredients,
+      steps,
+      idx
+    );
+    returnArr.push(newRecipe);
+    idx++;
   });
 
   return returnArr;
@@ -53,12 +59,11 @@ export const snapshotToFoodArray = snapshot => {
   });
 
   return returnArr;
-}
+};
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class AuthService {
   public user: User;
   private recipes = [];
@@ -67,13 +72,16 @@ export class AuthService {
   private foodDB = firebase.database().ref('foodSheet/');
   private username: string;
 
-  constructor(public afAuth: AngularFireAuth, public router: Router, 
-    public navCtrl: NavController) {
-      this.afAuth.authState.subscribe(user => {
+  constructor(
+    public afAuth: AngularFireAuth,
+    public router: Router,
+    public navCtrl: NavController
+  ) {
+    this.afAuth.authState.subscribe(user => {
       if (user) {
         this.user = user;
-          localStorage.setItem('user', JSON.stringify(this.user));
-          this.username = localStorage.getItem(this.user.email);
+        localStorage.setItem('user', JSON.stringify(this.user));
+        this.username = localStorage.getItem(this.user.email);
         this.navCtrl.navigateRoot(['./home']);
       } else {
         localStorage.setItem('user', null);
@@ -106,7 +114,6 @@ export class AuthService {
     await firebase.auth().signOut();
     localStorage.removeItem('user');
     this.navCtrl.navigateRoot(['./login']);
-
   }
 
   // ask firebase if we have a user logged in currently.
@@ -116,31 +123,32 @@ export class AuthService {
     } else {
       return false;
     }
-	}
-	
-	async signup(firstname: string,lastname:string,email: string, password: string){
-		try{
-      await this.afAuth.auth.createUserWithEmailAndPassword(email, password)
-      this.confirm_Signup(email);
-      this.accountFirstLastName(firstname, lastname);
-      
-      this.router.navigate(['./login']);
-
-		}catch(e){
-      alert("Error!"+e.message);
-		}
   }
 
+  async signup(
+    firstname: string,
+    lastname: string,
+    email: string,
+    password: string
+  ) {
+    try {
+      await this.afAuth.auth.createUserWithEmailAndPassword(email, password);
+      this.confirm_Signup(email);
+      this.accountFirstLastName(firstname, lastname);
 
-  accountEmail(): string  {
-    return  this.user.email ;
+      this.router.navigate(['./login']);
+    } catch (e) {
+      alert('Error!' + e.message);
+    }
+  }
+
+  accountEmail(): string {
+    return this.user.email;
   }
 
   accountUserName(): string {
     return this.username;
   }
-
-  
 
   resetPassword(new_password: string, conf_password: string) {
     if (this.afAuth.auth.confirmPasswordReset(new_password, conf_password)) {
@@ -154,10 +162,9 @@ export class AuthService {
     this.user.sendEmailVerification();
   }
 
-    accountFirstLastName(firstname: string, lastname: string): void {
-        this.username = firstname + lastname;
-    }
-
+  accountFirstLastName(firstname: string, lastname: string): void {
+    this.username = firstname + lastname;
+  }
 
   get getRecipes(): Array<Recipe> {
     return this.recipes;
