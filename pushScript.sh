ff5e31280eab7b30b18b6edb78b171f8ec303b9b
Austin Foy <austinfoy@u.boisestate.edu>
2019-04-16 13:59:51 -0600

    git master*@10c83e09

--- pushScript.sh
+++ pushScript.sh
@@ -1,5 +1,19 @@
-			current_Branch=$1
-			message=$2
+function parse_git_dirty() {
+	  git diff --quiet --ignore-submodules HEAD 2>/dev/null; [ $? -eq 1 ] && echo "*"
+  }
+
+# gets the current git branch
+function parse_git_branch() {
+	  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
+  }
+
+# get last commit hash prepended with @ (i.e. @8a323d0)
+function parse_git_hash() {
+	  git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/@\1/"
+  }
+			GIT_BRANCH=$(parse_git_branch)$(parse_git_hash)
+			message=$1
+			echo "$GIT_BRANCH"
 			git add .
-			git commit -m "$message $current_Branch"
-			git pull --rebase												     git push origin $current_Branch
+			git commit -m "$message $GIT_BRANCH"
+			git pull --rebase												     git push origin $GIT_BRANCH
