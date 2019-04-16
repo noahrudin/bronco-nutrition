1feb7d59884455f06bf1c899abcf64f003712dde
Austin Foy <austinfoy@u.boisestate.edu>
2019-04-16 14:01:10 -0600

    git master* @2035f262

--- pushScript.sh
+++ pushScript.sh
@@ -11,9 +11,11 @@ function parse_git_branch() {
 function parse_git_hash() {
 	  git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/@\1/"
   }
-			GIT_BRANCH=$(parse_git_branch)$(parse_git_hash)
+			GIT_BRANCH=$(parse_git_branch)
+			GIT_HASH=$(parse_git_hash)
 			message=$1
 			echo "$GIT_BRANCH"
 			git add .
-			git commit -m "$message $GIT_BRANCH"
-			git pull --rebase												     git push origin $GIT_BRANCH
+			git commit -m "$message $GIT_BRANCH $GIT_HASH"
+			git pull --rebase												     
+			git push origin $GIT_BRANCH
