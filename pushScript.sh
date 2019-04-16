c332467d0f546e1a1030afdbe3fa602109000636
Austin Foy <austinfoy@u.boisestate.edu>
2019-04-16 13:54:57 -0600

    capacitor.config.json angular.json

--- pushScript.sh
+++ pushScript.sh
@@ -1,6 +1,5 @@
-$current_Branch=git branch
-	message=$1
-		git add .
+			current_Branch=$1
+			message=$2
+			git add .
 			git commit -m "$message $current_Branch"
-				git pull --rebase
-					git push origin $current_Branch
+			git pull --rebase												     git push origin $current_Branch
