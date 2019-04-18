39777dc815199684b0d812e19dde4d14432a7b49
Austin Foy <austinfoy@u.boisestate.edu>
2019-04-18 09:52:14 -0600

    (Added a more selective git add) => branch:master hash:@3b0a44f8

--- pullScript.sh
+++ pullScript.sh
@@ -1,40 +1,4 @@
 #!/bin/bash
-
-# checks if branch has something pending
-function parse_git_dirty() {
-  git diff --quiet --ignore-submodules HEAD 2>/dev/null;
-}
-
-# gets the current git branch
-function parse_git_branch() {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
-}
-
-# get last commit hash prepended with @ (i.e. @8a323d0)
-function parse_git_hash() {
-  git rev-parse --short HEAD 2> /dev/null | sed "s/\(.*\)/@\1/"
-}
-
-function box_out()
-{
-  local s="$(parse_git_branch)=>$(parse_git_hash)"
-  tput setaf 3
-  echo " -${s//?/-}-
-| ${s//?/ } |
-| $(tput setaf 4)$s$(tput setaf 3) |
-| ${s//?/ } |
- -${s//?/-}-"
-  tput sgr 0
-}
-
-	GIT_BRANCH=$(parse_git_branch)
-	GIT_HASH=$(parse_git_hash)
-	box_out
-	git add .
-	git commit -m "pulling from current branch: $GIT_BRANCH and last hash: $GIT_HASH"
-	git pull --rebase
-	git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"
-	
 	
 	# checks if branch has something pending
 function parse_git_dirty() {
@@ -66,7 +30,13 @@ function box_out()
 	GIT_BRANCH=$(parse_git_branch)
 	GIT_HASH=$(parse_git_hash)
 	box_out
-	git add .
+	if git diff-index --quiet HEAD --; then
+  git commit -m "pulling from current branch: $GIT_BRANCH and last hash: $GIT_HASH"
+	git pull --rebase
+else
+    git add -i
 	git commit -m "pulling from current branch: $GIT_BRANCH and last hash: $GIT_HASH"
 	git pull --rebase
+fi
+
 	git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"
--- pushScript.sh
+++ pushScript.sh
@@ -30,9 +30,15 @@ function box_out()
 	GIT_BRANCH=$(parse_git_branch)
 	GIT_HASH=$(parse_git_hash)
 	message=$1
-	git add .
-	git commit -m "($message) branch:$GIT_BRANCH hash:$GIT_HASH"
+	box_out
+	if git diff-index --quiet HEAD --; then
+	git commit -m "($message) => branch:$GIT_BRANCH hash:$GIT_HASH"
 	git push origin $GIT_BRANCH
+else
+   git add -i
+	 git commit -m "($message) => branch:$GIT_BRANCH hash:$GIT_HASH"
+	git push origin $GIT_BRANCH
+fi
 		git log --graph --full-history --all --color --pretty=format:"%x1b[31m%h%x09%x1b[32m%d%x1b[0m%x20%s"
 	
 
